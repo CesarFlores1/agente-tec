35304068a3ac7aae993e2a0d68cfe22708edb915
CesarFlores1 <a01568517@tec.mx>
2022-11-28 18:05:59 -0600

    28/11/2022

--- Reto.py
+++ Reto.py
@@ -1,4 +1,4 @@
-#from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
 #import json, logging, os, atexit
 
 from mesa.visualization.ModularVisualization import ModularServer
